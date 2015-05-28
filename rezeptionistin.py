a7f202c925616aeee5bdcc31cb486eee39de6248
Daniel Danner <daniel@danner.de>
2015-05-28 10:42:37 +0200

    Disable some K4CG-specific commands

--- rezeptionistin.py
+++ rezeptionistin.py
@@ -85,19 +85,19 @@ def on_msg(self, nick, host, channel, message):
   if message.lower().startswith('!help'):
     send_message(self, nick, "Erzaehl mir doch was du brauchst, mein Junge.")
     send_message(self, nick, "Ich kann bisher:")
-    send_message(self, nick, "!kt - Zeige aktuelle Temperatur in der K4CG.")
+    # send_message(self, nick, "!kt - Zeige aktuelle Temperatur in der K4CG.")
     send_message(self, nick, "!gt - Guten Tag wuenschen.")
-    send_message(self, nick, "!np - Dir sagen welche Musik so laeuft.")
+    # send_message(self, nick, "!np - Dir sagen welche Musik so laeuft.")
     send_message(self, nick, "!beleidige <nick> - Jemanden beleidigen.")
     send_message(self, nick, "!lobe <nick> - Jemandem ein Kompliment machen.")
-    send_message(self, nick, "oder dir den Titel von URLs sagen die du in den Channel postest")
-  if message.lower().startswith('!kt'):
-    temp = netcat("2001:a60:f073:0:21a:92ff:fe50:bdfc", 31337, "9001")
-    send_message(self, channel, "Die aktuelle Temperatur in der K4CG ist{temp} Grad".format(temp=temp) )
+    # send_message(self, nick, "oder dir den Titel von URLs sagen die du in den Channel postest")
+  # if message.lower().startswith('!kt'):
+    # temp = netcat("2001:a60:f073:0:21a:92ff:fe50:bdfc", 31337, "9001")
+    # send_message(self, channel, "Die aktuelle Temperatur in der K4CG ist{temp} Grad".format(temp=temp) )
   if message.lower().startswith('!gt'):
     send_message(self, channel, "Ich lebe noch, {nick}".format(nick=nick))
-  if message.lower().startswith('!np'):
-    send_message(self, channel, "Das funktioniert noch nicht.")
+  # if message.lower().startswith('!np'):
+    # send_message(self, channel, "Das funktioniert noch nicht.")
   if message.lower().startswith('!beleidige'):
     if len(message.split()) >= 2:
       send_message(self, channel, message.split()[1] + ", du " + random.choice(list(open('lists/insults.txt'))))
